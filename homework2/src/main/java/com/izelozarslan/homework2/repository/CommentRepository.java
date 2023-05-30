375c3065457d286c280e41a7ecba42921c98cbec
izelozarslan <izel.ozarslan@gmail.com>
2023-05-30 18:27:06 +0300

    Comment apis added

--- homework2/src/main/java/com/izelozarslan/homework2/repository/CommentRepository.java
+++ homework2/src/main/java/com/izelozarslan/homework2/repository/CommentRepository.java
@@ -2,6 +2,13 @@ package com.izelozarslan.homework2.repository;
 
 import com.izelozarslan.homework2.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//public interface CommentRepository extends JpaRepository<Comment, Long> {
-//}
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findCommentsByUserId(Long userId);
+    List<Comment> findCommentsByProductId(Long productId);
+}
